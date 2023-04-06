OffBroadway/cubeboot

custom menu over Options menu

cubeboot/source/helpers.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #include <stdbool.h>
-#include <gcbool.h>
 #include <ogc/system.h>
 #include <ogc/lwp_watchdog.h>
 #include <ogc/machine/processor.h>
@@ -42,4 +41,10 @@ int ensdwith(const char *str, const char *suffix) {
     if (lensuffix >  lenstr)
         return 0;
     return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+size_t memlen(char **arr) {
+    size_t k;
+    for(k = 0; arr[k] != NULL; k++);
+    return k;
 }

cubeboot/source/helpers.h
@@ -10,3 +10,4 @@ extern void udelay(int us);
 u32 generate_random_color();
 bool is_dolphin();
 int ensdwith(const char *str, const char *suffix);
+size_t memlen(char **arr);

cubeboot/source/loader.c
@@ -3,6 +3,7 @@
 #include <malloc.h>
 
 #include "sd.h"
+#include "helpers.h"
 
 #include "crc32.h"
 #include "print.h"
@@ -87,5 +88,13 @@ void load_program() {
     // No stack - we need it all
     AR_Init(NULL, 0);    
 
-    DOLtoARAM(dol_buf, 0, NULL);
+    static char *argv[] = {
+        "boot.dol",
+        // "Autoload=gcldr:/XD.iso",
+        // "Autoboot=Yes",
+        NULL,
+    };
+
+    size_t argc = memlen(argv);
+    DOLtoARAM(dol_buf, argc, argv);
 }

Ma-Rang/cubiboot-for-dolphin-director

cubeboot/source/helpers.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #include <stdbool.h>
-#include <gcbool.h>
 #include <ogc/system.h>
 #include <ogc/lwp_watchdog.h>
 #include <ogc/machine/processor.h>
@@ -42,4 +41,10 @@ int ensdwith(const char *str, const char *suffix) {
     if (lensuffix >  lenstr)
         return 0;
     return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+size_t arrlen(char **arr) {
+    size_t k;
+    for(k = 0; arr[k] != NULL; k++);
+    return k;
 }

cubeboot/source/helpers.h
@@ -10,3 +10,4 @@ extern void udelay(int us);
 u32 generate_random_color();
 bool is_dolphin();
 int ensdwith(const char *str, const char *suffix);
+size_t arrlen(char **arr);

cubeboot/source/loader.c
@@ -5,6 +5,7 @@
 #include "sd.h"
 #include "settings.h"
 #include "state.h"
+#include "helpers.h"
 
 #include "crc32.h"
 #include "print.h"
@@ -179,5 +180,13 @@ void boot_program(char *alternative_path) {
     VIDEO_WaitVSync();
 #endif
 
-    DOLtoARAM(dol_buf, params.argc, params.argv);
+    static char *argv[] = {
+        "boot.dol",
+        // "Autoload=gcldr:/XD.iso",
+        // "Autoboot=Yes",
+        NULL,
+    };
+
+    size_t argc = arrlen(argv);
+    DOLtoARAM(dol_buf, argc, argv);
 }